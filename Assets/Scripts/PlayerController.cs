45610d902f4cd5fc7a371facd7df11e6121e74fa
artjom105 <37247163+artjom105@users.noreply.github.com>
2020-02-08 18:48:33 +0100

    cube control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController characterController;
+    [System.NonSerialized]
+    public Vector3 moveVector;
+    [System.NonSerialized]
+    public float moveHorizontal;
+    [System.NonSerialized]
+    public float moveVerctival;
+    [SerializeField]
+    public float force = 10.0f;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVerctival = Input.GetAxis("Vertical");
+        moveVector = new Vector3(moveHorizontal,moveVerctival,0.0f);
+        characterController.Move(moveVector);
+    }
+}
