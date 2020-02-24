0a23ac87bcbcd55c42de1f035cc222337f919a4a
artjom105 <37247163+artjom105@users.noreply.github.com>
2020-02-24 22:31:51 +0100

    some cc stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,9 @@ public class PlayerController : MonoBehaviour
     public float moveVerctival;
     [SerializeField]
     public float force = 10.0f;
+
+    [SerializeField]
+    public float damping = 0.5f;
     // Start is called before the first frame update
     void Start(){
         
@@ -20,9 +23,17 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
-        moveHorizontal = Input.GetAxis("Horizontal");
-        moveVerctival = Input.GetAxis("Vertical");
+        moveHorizontal = Input.GetAxis("Horizontal");  
+        moveVerctival = Input.GetAxis("Jump");      
         moveVector = new Vector3(moveHorizontal,moveVerctival,0.0f);
-        characterController.Move(moveVector);
+        if(characterController.isGrounded){
+            characterController.Move(moveVector);
+        }
+        if(!characterController.isGrounded){            
+            moveVector += Physics.gravity * Time.deltaTime;            
+            moveVector.x = moveVector.x/2;
+            characterController.Move(moveVector); 
+        }
+
     }
 }
